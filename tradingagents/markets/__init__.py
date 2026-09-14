34db4479df4200d32b313e988dc02072408d594c
coolman1984 <54982309+coolman1984@users.noreply.github.com>
2026-09-14 19:20:45 +0300

    fix(egx): remove circular package import

--- tradingagents/markets/__init__.py
+++ tradingagents/markets/__init__.py
@@ -6,13 +6,10 @@ from .egypt import (
     is_egx_ticker,
     normalize_egx_ticker,
 )
-from .egypt_config import assert_egypt_recommendation_ready, build_egypt_config
 
 __all__ = [
     "EGYPT_MARKET",
     "EgyptMarketProfile",
-    "assert_egypt_recommendation_ready",
-    "build_egypt_config",
     "is_egx_ticker",
     "normalize_egx_ticker",
 ]
